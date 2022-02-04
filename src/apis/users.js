e40c496d966c40b02280384fb1002590e080e100
Nicolanz <zarateguarinnicolasandres@gmail.com>
2022-02-03 23:50:58 -0500

    User model and validations

---
+++ src/apis/users.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const router = Router();
+
+/**
+ * @description To create a new User Acoount
+ * @access Public
+ * @api /users/api/resgister
+ * @type POST
+ */
+
+export default router;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log("Hola Mundo");
+// Import dependencies
+import express, { Router } from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import consola from 'consola';
+
+// Import app env vars
+import { DB, PORT } from './constants/index';
+
+// Import Router
+import userApis from "./apis/users";
+
+// Init app object
+const app = express();
+
+// app.use is called every time a request in sent to the server
+
+// Use middlewares
+// CORS used to allow conectivity among 2 servers
+app.use(cors());
+// Parser to get the req in a json obj
+app.use(express.json());
+// inject sub router and apis
+app.use('/users', userApis);
+
+// Main funtion 
+const main = async() => {
+    try {
+        // conmect to mongo db (user-auth)
+        await mongoose.connect(DB);
+        consola.success('Base de datos conectada con exito!');
+        app.listen(PORT, () => {
+            consola.success(`Servidor iniciado en el puerto: ${PORT}`);
+        });
+    } catch (e) {
+        // print in case of error
+        consola.error(`Imposible iniciar el servidor \n${e.message}`);
+    }
+};
+main();
