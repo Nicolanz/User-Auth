acafe53fe3ee08de253394a762a01e31b34b2148
Nicolanz <zarateguarinnicolasandres@gmail.com>
2022-02-18 04:22:10 -0500

    verficate password

--- src/apis/users.js
+++ src/apis/users.js
@@ -6,7 +6,7 @@ import { DOMAIN } from "../constants";
 import sendMail from "../functions/email-sender";
 import { userAuth } from "../middlewares/auth-guard";
 import Validator from "../middlewares/validator-middleware";
-import { AuthenticateValidations, RegisterValidations } from "../validators";
+import { AuthenticateValidations, RegisterValidations, resetPasswordValidator } from "../validators";
 
 
 const router = Router();
@@ -145,8 +145,7 @@ router.post("/api/authenticate", AuthenticateValidations, Validator, async (req,
         return res.status(500).json({
             success: false,
             message: "An error ocurred",
-        })
-
+        });
     }
 })
 
@@ -161,6 +160,135 @@ async (req, res)=>{
     return res.status(200).json({
         user: req.user
     });
+});
+
+/**
+ * @description To initiate the password reset process
+ * @api /users/api/reset-password
+ * @access Public
+ * @type POST
+ */
+router.put("/api/reset-password", resetPasswordValidator, Validator, async(req, res) => {
+
+    try {
+
+        let { email } = req.body;
+        let user = await User.findOne({email});
+        if (!user){
+            return res.status(404).json({
+                success: false,
+                message: "User with this email is not found",
+            });
+        }
+
+        user.generatePasswordReset();
+        await user.save();
+
+        let html = `
+                <div>
+                    <h1>Hello, ${user.username}</h1>
+                    <p>Please click the following link to reset your password</p>
+                    <p>If this password reset request is not created by your then your can ignore this email</p>
+                    <a href="${DOMAIN}users/reset-password-now/${user.resetPasswordToken}">Verify Now</a>
+                </div>
+        `;
+        sendMail(
+            user.email,
+            "Reset Password",
+            "Please reset your password",
+            html,
+        );
+
+        // send password reset link
+        return res.status(200).json({
+            success: true,
+            message: "Password reset link is sent to your email",
+        });
+
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "An error ocurred",
+        });
+    }
+});
+
+/**
+ * @description To render reset password process
+ * @api /users/reset-password-now/:resetPasswordToken
+ * @access Restricted via email
+ * @type GET
+ */ 
+router.get("/reset-password-now/:resetPasswordToken", async(req, res)=>{
+    try {
+        let { resetPasswordToken } = req.params;
+        let user = await User.findOne({
+            resetPasswordToken,
+            resetPasswordExpiresIn: { $gt:Date.now() }});
+        
+        if (!user){
+            return res.status(401).json({
+                success: false,
+                message: "Password reset token is invalid or has expired",
+            });
+        }
+        return res.sendFile(join(__dirname, "../templates/password-reset.html"));
+    } catch (err){
+        return res.sendFile(join(__dirname, "../templates/errors.html"));
+    }
+});
+
+/**
+ * @description To reset the password
+ * @api /users/api/reset-password-now
+ * @access Restricted via email
+ * @type POST
+ */ 
+router.post("/api/reset-password-now", async (req, res)=>{
+    try{
+        let { resetPasswordToken, password } = req.body;
+        let user = await User.findOne({
+            resetPasswordToken,
+            resetPasswordExpiresIn: { $gt:Date.now() }});
+        
+        if (!user){
+            return res.status(401).json({
+                success: false,
+                message: "Password reset token is invalid or has expired",
+            });
+        }
+        user.password = password;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpiresIn = undefined;
+
+        await user.save();
+
+        // send notficiation about correctly change of password
+        let html = `
+                <div>
+                    <h1>Hello, ${user.username}</h1>
+                    <p>Your password is resetted successfuly</p>
+                    <p>If this reset password is not done by you then you can contact our team</p>
+                </div>
+        `;
+        sendMail(
+            user.email,
+            "Reset Password Sucessfully",
+            "Your password is changed",
+            html,
+        );
+
+        return res.status(200).json({
+            success: true,
+            message: "Your password reset request is complete and your password is resetted successfully. Login into your new account"
+        })
+
+    } catch ( err ){
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong"
+        })
+    }
 });
 
 export default router;
